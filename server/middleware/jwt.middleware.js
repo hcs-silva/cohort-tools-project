82bc319cc12ef4d2ea970d4198e18d0532cad353
nehais <nehasinha82@gmail.com>
2025-01-16 19:01:12 +0100

    JWT Middleware added

---
+++ server/middleware/jwt.middleware.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+const isAuthenticated = (req, res, next) => {
+  try {
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.split(" ")[0] == "Bearer"
+    ) {
+      const token = req.headers.authorization.split(" ")[1];
+      const payload = jwt.verify(token, process.env.TOKEN_SECRET);
+
+      console.log("Payload on verify", payload);
+      req.payload = payload;
+      next();
+    }
+  } catch (error) {
+    console.log("Invalid token", error);
+    res.status(401).json({ errorMessage: error });
+  }
+};
+
+module.exports = { isAuthenticated };
